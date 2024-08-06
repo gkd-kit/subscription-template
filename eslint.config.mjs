9582418591e1fcf5e92d4a333ac8520d2e3fc190
lisonge <i@songe.li>
2024-08-06 17:47:36 +0800

    perf: update eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      quotes: ['error', 'single', { allowTemplateLiterals: false }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'error',
+    },
+  },
+);
